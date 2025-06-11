8998bbe1922c90fc3be23393971a1da33448ecca
afifnaufal04 <afifnaufal447@gmail.com>
2025-06-11 18:08:15 +0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+registerSW();
+
+// Registers a service worker
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      // Change the service worker URL to see what happens when the SW doesn't exist
+      const registration = await navigator.serviceWorker.register("sw.js"); 
+      console.log("Service Worker registered");      
+    } catch (error) {
+      showResult("Error while registering: " + error.message);
+    }    
+  } else {
+      showResult("Service workers API not available");
+  }
+}; 
+
+function showResult(text) {
+  document.querySelector("output").innerHTML = text;
+}
---
+++ sw.js
@@ -0,0 +1,64 @@
+self.addEventListener("install", async event => {
+  const cache = await caches.open("pwa-assets");
+  // it stores all resources on first SW install
+  cache.addAll([
+    "/",
+    "index.html",
+    "dokter.html",
+    "departemen.html",
+    "app.js",
+    "assets/css/style.css",
+    "assets/img/favicon.png",
+    "assets/img/hero-bg.jpg",
+    "assets/img/about.jpg",
+    "assets/img/testimonials/testimonials-1.jpg",
+    "assets/img/testimonials/testimonials-2.jpg",
+    "assets/img/testimonials/testimonials-3.jpg",
+    "assets/img/testimonials/testimonials-4.jpg",
+    "assets/img/testimonials/testimonials-5.jpg",
+    "assets/img/icon/facebook.png",
+    "assets/img/icon/instagram.png",
+    "assets/img/icon/linkedin.png",
+    "assets/img/service/service-1.jpg",
+    "assets/img/service/service-2.jpg",
+    "assets/img/service/service-3.jpg",
+    "assets/img/service/service-4.jpg",
+    "assets/img/service/service-5.jpg",
+    "assets/img/service/service-6.jpg",
+    "assets/img/doctors/doctors-1.jpg",
+    "assets/img/doctors/doctors-2.jpg",
+    "assets/img/doctors/doctors-3.jpg",
+    "assets/img/doctors/doctors-4.jpg",
+    "assets/img/doctors/doctors-5.jpg",
+    "assets/img/doctors/doctors-6.jpg",
+    "assets/img/departemen/departments-1.jpg",
+    "assets/img/departemen/departments-2.jpg",
+    "assets/img/departemen/departments-3.jpg",
+    "assets/img/departemen/departments-4.jpg",
+    "assets/img/departemen/departments-5.jpg"
+  
+  ]); 
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(name => name !== "pwa-assets")
+          .map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+
+self.addEventListener("fetch", event => {
+   event.respondWith(
+     caches.match(event.request).then(cachedResponse => {
+	   // It can update the cache to serve updated content on the next request
+         return cachedResponse || fetch(event.request);
+     }
+   )
+  )
+});
